bbf694167aa10d00628253db88bca6638bdd67d0
Luis Cova <luis.cova@bigbox.com.ar>
2021-12-06 00:29:30 -0300

    feat: act_4_registro_de_personas app

---
+++ Booklet/act_4_registro_de_personas/app.js
@@ -0,0 +1,10 @@
+alert("Se le solicitara los 5 nombres de la familia Simpson");
+let mama = prompt("Nombre de la mama");
+let papa = prompt("Nombre del papa");
+let hijoMayor = prompt("Nombre del hijo mayor");
+let hijaMayor = prompt("Nombre de la hija mayor");
+let hijaMenor = prompt("Nombre de la hija menor");
+
+let familia = `Mama: ${mama} \nPapa: ${papa} \nHijo Mayor: ${hijoMayor} \nHija Mayor: ${hijaMayor} \nHija Menor: ${hijaMenor}`;
+
+alert(familia);
