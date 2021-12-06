6dce2624551e62fdffc85f37780c11c5de06fa04
Luis Cova <luis.cova@bigbox.com.ar>
2021-12-05 23:44:21 -0300

    feat: act_2_ciudades app

---
+++ Booklet/act_2_ciudades/app.js
@@ -0,0 +1,11 @@
+const city_1 = "Buenos Aires";
+const city_2 = "Salta";
+const city_3 = "Rosario";
+const city_4 = "San Miguel de Tucúman";
+const city_5 = "Mendoza";
+
+console.log(city_1)
+console.log(city_2)
+console.log(city_3)
+console.log(city_4)
+console.log(city_5)
