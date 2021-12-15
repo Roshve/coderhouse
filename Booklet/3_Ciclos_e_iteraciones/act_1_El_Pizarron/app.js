30ee30cc01c9ead97d218f7c62641f48f63ff1f3
Luis Cova <luis.cova@bigbox.com.ar>
2021-12-15 20:44:02 -0300

    feat: booklet 3 loops and iteration

---
+++ Booklet/3_Ciclos_e_iteraciones/act_1_El_Pizarron/app.js
@@ -0,0 +1,8 @@
+let number = prompt("Ingresa un número")
+let text = prompt("Ingresa un texto")
+let i = 0;
+
+while (i <= number) {
+    alert(text);
+    i ++;
+};
--- Proyectos/3_algoritmo_ciclos/app.js
+++ Proyectos/3_algoritmo_ciclos/app.js
@@ -1,3 +1,11 @@
-/* 2. Conceptos generales: sintaxis y variables
+/* 3. Conceptos generales: Ciclos
     Luis Alejandro Cova Ascanio
 */
+let number = prompt("Ingresa un número")
+let text = prompt("Ingresa un texto")
+let i = 0;
+
+while (i <= number) {
+    alert(text);
+    i ++;
+};
