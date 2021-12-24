98ad8d32f7b672ec9735d1c4c51d6d0bf507c136
Luis Cova <luis.cova@bigbox.com.ar>
2021-12-24 19:51:01 -0300

    style: 4_algoritmo_funcional

--- Proyectos/4_algoritmo_funciones/app.js
+++ Proyectos/4_algoritmo_funciones/app.js
@@ -1,12 +1,21 @@
 /* 3. Conceptos generales: Funciones
     Luis Alejandro Cova Ascanio
 */
-/* let number = prompt("Ingresa un número")
 
-
-function ConversionDeNumero(valor) {
-    valor = parseInt(valor) + 20
-    return alert(`Tu numero convertido es el siguiente ${valor}`)
+function ask(question) {
+    return prompt(question)
 }
 
-ConversionDeNumero(number); */
+function menu() {
+    const opcion = ask("Bienvenido al MasRespuesto.com para poder ofrecerle un producto de primera calidad uses las opciones del menu para saber lo que desea \n\n1) Comprar Respuestos de Autos\n2) Contactanos\n3) Cotizador")
+    
+}
+
+menu()
+const birthday = ask("Porfavor ingrese su año de nacimiento")
+const firstName = ask("Porfavor ingrese su primer nombre")
+const country = ask("Porfavor ingrese su pais de locación")
+
+console.log(birthday)
+console.log(firstName)
+console.log(country)
