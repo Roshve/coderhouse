145628d7707928ea2d31a45cab51adf0dae339ab
Luis Cova <luis.cova@bigbox.com.ar>
2021-12-28 22:45:31 -0300

    feat: new proyect arrays

---
+++ Proyectos/6_algoritmo_arrays_complem/store.js
@@ -0,0 +1,26 @@
+/* class SingIn {
+    constructor(username, password, firtsName, lastName) {
+        this.username = username.toString();
+        this.password = password.toString();
+        this.firtsName = firtsName;
+        this.lastName = lastName;
+        this.agree = false;
+    }
+
+    check() {
+        desagree = prompt("Escriba 1 si esta deacuerdo con los terminos y condiciones")
+        if(desagree === 1) {
+            this.agree = true
+        } else {
+            alert("vuelva a iniciar")
+        }
+    }
+}
+
+const user1 = new SingIn(
+    prompt("Ingrese su username"),
+    prompt('ingrese su contraseña'),
+    prompt('ingrese su primer nombre'),
+    prompt('Ingrese su apellido'),
+)
+user1.check() */
