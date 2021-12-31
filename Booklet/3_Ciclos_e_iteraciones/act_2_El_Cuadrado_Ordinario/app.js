b20b287927dd455271f4d6096a981a98a31acff2
Luis Cova <luis.cova@bigbox.com.ar>
2021-12-31 16:31:41 -0300

    fix: lin

--- Booklet/3_Ciclos_e_iteraciones/act_2_El_Cuadrado_Ordinario/app.js
+++ Booklet/3_Ciclos_e_iteraciones/act_2_El_Cuadrado_Ordinario/app.js
@@ -1,8 +1,8 @@
 let number = prompt("Usuario te ordeno un numero")
 
 for (let index = 0; index < number; index++) {
-    if (index > 3) {
-        break
-    };
-    alert("lado");
+  if (index > 3) {
+    break
+  };
+  alert("lado");
 }
