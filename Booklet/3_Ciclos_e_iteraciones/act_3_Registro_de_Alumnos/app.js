fc34fee7baa830174ad3c76b310faa4430b4bf10
Luis Cova <luis.cova@bigbox.com.ar>
2021-12-31 17:10:58 -0300

    fix: registro de alumnos

--- Booklet/3_Ciclos_e_iteraciones/act_3_Registro_de_Alumnos/app.js
+++ Booklet/3_Ciclos_e_iteraciones/act_3_Registro_de_Alumnos/app.js
@@ -1,5 +1,6 @@
-let student = prompt("Usuario te pido que ingreses los estudiantes que tengas a tu cargo")
-/* 
-for () {
-    
-} */
+let students = ""
+
+for (index = 0; index < 10; index++) {
+  students += prompt("Usuario te pido que ingreses los estudiantes que tengas a tu cargo")+"\n"
+}
+alert(students)
